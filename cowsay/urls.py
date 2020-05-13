54c424f82ea39b9c40547e70d4bca4d0c40da954
Chelsea White <chewh115@gmail.com>
2020-05-13 18:24:15 -0400

    Does necessary hookups to have app running pre-logic

--- cowsay/urls.py
+++ cowsay/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django_cowsay.urls import urlpatterns as cowsayurls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+urlpatterns += cowsayurls
--- django_cowsay/admin.py
+++ django_cowsay/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from django_cowsay.models import CowSayText
 
 # Register your models here.
+admin.site.register(CowSayText)
